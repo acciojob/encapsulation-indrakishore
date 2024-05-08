9341dc403198862925216695b408d3804b13addf
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2024-05-08 18:59:19 +0530

    Identification of error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly rwOnlyObj = new RWOnly();
+        System.out.println(rwOnlyObj.variableName = "Accio");
+    }
   
 }
